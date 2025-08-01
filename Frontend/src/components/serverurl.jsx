6b35dbe5112a200856a63a2ca302e9a1cb272190
Umair Zakria <tomrichard872@gmail.com>
2025-08-02 04:34:26 +0500

    Update serverurl.jsx

--- Frontend/src/components/serverurl.jsx
+++ Frontend/src/components/serverurl.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 // const serverurl = 'http://localhost:3001'
 
-// const serverurl = 'http://localhost:5000'
+const serverurl = 'http://localhost:5000'
 
-const serverurl = 'https://server2-kohl.vercel.app'
+
 
 
 
 export default serverurl
+
